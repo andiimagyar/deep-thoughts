882ba4baf454b25fdad453e8ce9dfc0e4b95a9ef
Andii <andii.magyar@gmail.com>
2021-06-17 19:46:27 -0700

    mutations

--- server/server.js
+++ server/server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-
+const {authMiddleware } = require('./utils/auth')
 const { ApolloServer } = require('apollo-server-express')
 const { typeDefs, resolvers } = require('./schemas')
 
@@ -10,7 +10,8 @@ const app = express();
 
 const server = new ApolloServer({
   typeDefs,
-  resolvers
+  resolvers,
+  context: authMiddleware
 });
 
 server.applyMiddleware({ app });
